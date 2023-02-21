401cd67d7e9550e675f137275b78c3632bdc9577
Yingxin Zhang <s3752708@student.rmit.edu.au>
2023-02-21 22:19:38 +1100

    add the api requests

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 function BookEdit({ book, onSubmit }) {
   const [title, setTitle] = useState(book.title);
 
-  const handleChange =(event)=>{
+  const handleChange = (event) => {
     setTitle(event.target.value);
   };
 
-  const handleSubmit =(event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     onSubmit(book.id, title);
-  }
-  return (<form onSubmit={handleSubmit} className="book-edit">
-    <label>Title</label>
-    <input className="input" value={title} onChange={handleChange} />
-    <button className="button is-primary">Save</button>
-  </form>
+  };
+  return (
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
   );
 }
 export default BookEdit;
